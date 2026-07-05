4aed1dae1f2f9965dc600643eb74e1cb33bdadeb
DiegoH81 <hidal8909ins@gmail.com>
2026-07-04 19:58:42 -0500

    feat(embedding): add initial embedding

--- main.cu
+++ main.cu
@@ -2,10 +2,15 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#include <vector>
 
+#include "tensor.cuh"
 
 int main()
 {
+
+    Tensor testin({ 1, 2, 3, 4, 5, 6 });
+    testin.print();
     
     return 0;
 }
